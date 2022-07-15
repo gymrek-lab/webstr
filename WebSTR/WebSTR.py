4444ce71ddbb32588f2b237a604e68223ea50672
Melissa Gymrek <melissagymrek@Melissas-MacBook-Pro.local>
2022-07-14 21:55:46 -0700

    changing to fastapi

--- WebSTR/WebSTR.py
+++ WebSTR/WebSTR.py
@@ -4,34 +4,28 @@ WebSTR v2 database application
 """
 
 import argparse
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.responses import FileResponse 
+import uvicorn
 
-#################### Database paths ###############
+#################### Set up the app ##########################
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-#################### Set up flask server ###############
-server = Flask(__name__)
-server.secret_key = 'WebSTR' 
+#################### Functions to render pages ##########################
 
-#################### Render region page ###############
-
-# TODO
-
-#################### Render locus page ###############
-
-# TODO
-
-#################### Render HTML pages ###############
-@server.route('/')
-@server.route('/WebSTR')
-def WebSTRHome():
-    return render_template('homepage.html')
+@app.get("/", response_class=HTMLResponse)
+async def WebSTRHome(request: Request):
+    return templates.TemplateResponse("homepage.html", {"request": request})
 
 #################### Set up and run the server ###############
-def main():
-    parser = argparse.ArgumentParser(__doc__)
-    parser.add_argument("--host", help="Host to run app", type=str, default="0.0.0.0")
-    parser.add_argument("--port", help="Port to run app", type=int, default=5000)
-    args = parser.parse_args()
-    server.run(debug=False, host=args.host, port=args.port)
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(__doc__)
+	parser.add_argument("--host", help="Host to run app", type=str, default="0.0.0.0")
+	parser.add_argument("--port", help="Port to run app", type=int, default=5000)
+	args = parser.parse_args()
+	uvicorn.run(app, host=args.host, port=args.port)
