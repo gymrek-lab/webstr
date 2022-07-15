290bfb9271047e577d09e988c6d12a63ac7724bf
Melissa Gymrek <melissagymrek@Melissas-MacBook-Pro.local>
2022-07-15 13:25:17 -0700

    adding empty region and locus pages

--- WebSTR/WebSTR.py
+++ WebSTR/WebSTR.py
@@ -22,6 +22,23 @@ templates = Jinja2Templates(directory="templates")
 async def WebSTRHome(request: Request):
     return templates.TemplateResponse("homepage.html", {"request": request})
 
+@app.get("/region/", response_class=HTMLResponse)
+@app.get("/region/{genome}/{query}", response_class=HTMLResponse)
+async def WebSTRRegion(request: Request, genome: str, query: str):
+	template_items = {"request": request, \
+	                "genome": genome, \
+					"query": query}
+	return templates.TemplateResponse("region.html", template_items)
+
+@app.get("/locus/", response_class=HTMLResponse)
+@app.get("/locus/{genome}/{trsetid}/{strid}", response_class=HTMLResponse)
+async def WebSTRLocus(request: Request, genome: str, trsetid: str, strid: str):
+	template_items = {"request": request, \
+	                "genome": genome, \
+	                "trsetid": trsetid,
+					"strid": strid}
+	return templates.TemplateResponse("locus.html", template_items)
+
 #################### Set up and run the server ###############
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(__doc__)
