453cb04d5a9bf18ef41bc70a60dd5a62b41c95d9
Ciara Reeve <77694019+ciarareeve@users.noreply.github.com>
2024-08-06 13:13:41 -0700

    adds new table design with control over click locations for sorting and tooltips

--- WebSTR/WebSTR.py
+++ WebSTR/WebSTR.py
@@ -37,15 +37,23 @@ def search():
     region_genome = request.args.get('genome')
     region_query = request.args.get('query').upper()
     region_data = GetRegionData(region_query, region_genome, BASEPATH)
-    if region_data.shape[0] == 0: return render_template('view2_nolocus.html')
+    if region_data.shape[0] == 0: 
+        return render_template('view2_nolocus.html')
+    
     gene_trace, gene_shapes, numgenes = GetGeneShapes(region_query, region_genome, BASEPATH)
     plotly_plot_json, plotly_layout_json = GetGenePlotlyJSON(region_data, gene_trace, gene_shapes, numgenes)
+    
+    # Convert DataFrame to dictionary
+    data = region_data.to_dict(orient='records')
+    print("Region Data (dictionary format):", data)
+    
     return render_template('view2.html',
-                           table = region_data.to_records(index=False),
-                           graphJSON = plotly_plot_json, layoutJSON = plotly_layout_json,
-                           chrom = region_data["chr"].values[0].replace("chr",""),
-                           strids = list(region_data["strid"]),
-                           genome = region_genome) 
+                           data=data,
+                           graphJSON=plotly_plot_json, 
+                           layoutJSON=plotly_layout_json,
+                           chrom=region_data["chr"].values[0].replace("chr",""),
+                           strids=list(region_data["strid"]),
+                           genome=region_genome)
 
 #################### Render locus page ###############
 @server.route('/locus')
@@ -147,7 +155,8 @@ def main():
         server.config.update(
             TEMPLATES_AUTO_RELOAD=True
         )
-    server.run(debug=FLASK_DEBUG, host=args.host, port=args.port)
+    # server.run(debug=FLASK_DEBUG, host=args.host, port=args.port)
+    server.run(debug=True, host=args.host, port=args.port)
 
 if __name__ == '__main__':
     main()
