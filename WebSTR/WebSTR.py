632b7392c525bbad674bb8a936903051b1b8ae81
ciarareeve <77694019+ciarareeve@users.noreply.github.com>
2024-08-13 14:39:18 -0700

    restore WebSTR.py debug settings

--- WebSTR/WebSTR.py
+++ WebSTR/WebSTR.py
@@ -152,7 +152,7 @@ def main():
         server.config.update(
             TEMPLATES_AUTO_RELOAD=True
         )
-    server.run(debug=True, host=args.host, port=args.port)
+    server.run(debug=FLASK_DEBUG, host=args.host, port=args.port)
 
 if __name__ == '__main__':
     main()
