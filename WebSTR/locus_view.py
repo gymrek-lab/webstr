7849a603d9dc3ed1a291604b2f750162de21c47b
Melissa Gymrek <melissagymrek@Melissas-MacBook-Pro-2.local>
2024-10-04 16:22:37 -0700

    handle missing sequence data

--- WebSTR/locus_view.py
+++ WebSTR/locus_view.py
@@ -319,7 +319,7 @@ def GetSeqDataAPI(repeat_id):
         sys.stderr.write("WARNING: could not access allseq table from API\n")
         return None
     df = pd.DataFrame.from_records(resp.json())
-    if df.shape[0] == 0:
+    if df.empty:
         return None
     agg_df = df.pivot_table(index='sequence', columns='population', values='frequency', aggfunc='first').reset_index()
     agg_df = agg_df.fillna(0)
